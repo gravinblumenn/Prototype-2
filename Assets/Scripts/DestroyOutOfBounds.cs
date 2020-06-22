f30acbd4fd377724ed2ebd8506765493ee044204
Gavin Blumenshein <gavinblumenn@gmail.com>
2020-06-22 13:51:12 -0600

    Initial Prototype 2 Commit
    Initial Protoype 2 Commit

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowerBound = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if object goes past players view, removes the object  
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    //These are Variables
+    private float horizontalInput;
+    private float speed = 10.0f;
+    private float xRange = 17;
+    public GameObject projectilePreFab;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //shoots Steak PreFab
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePreFab, transform.position, projectilePreFab.transform.rotation);
+            
+        }
+        //Keeps PLayer within a visible range
+        if  (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
+        //Player movement on the x-axis
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
